3168b0e9c30bdc2dc360be37d83a6b71654c9b12
Bradley Olson <lotrftw9@gmail.com>
2019-10-02 10:56:26 -0700

    BT225 Start: Set up throttle inputs on controller

---
+++ Source/BattleTank/Private/TankTrack.cpp
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TankTrack.h"
+
+void UTankTrack::SetThrottle(float Throttle) {
+	FString Name = GetName();
+	UE_LOG(LogTemp, Warning, TEXT("%s throttle: %f"), *Name, Throttle);
+};
+
